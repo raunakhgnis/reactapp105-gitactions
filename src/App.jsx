cbd9cb65b0c193b6874f33348d5bcab4da417b05
Raunak <raunakkumarsingh880@gmail.com>
2025-07-31 12:36:14 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
       <h1>CI/CD and Cloud DevOps course</h1>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
       <p>New Line</p>
+      <h1>Done with Git Page & Git Actions </h1>
+      
     </div>
   );
 }
